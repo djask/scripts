f11ab01a9c8f9e0ebe0967cd8aa114693f5fa638
ubuntu <ubuntu@ubuntu.gateway>
2018-07-27 05:36:57 +0000

    nfs script added

---
+++ mountnfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#mounts all directories from specified host
+#./nfsmount [host] [mountpath]
+#unmount is umount -a -t nfs
+
+showmount -e $1 | awk '{if(NR>1)print}' | sed "s/\*//g" | while read -r export ; do
+	mountdir="$2/$(echo $1$export | sed -e 's/\//_/g')"
+        if [ ! -d "$mountdir" ]; then
+                mkdir -p "$mountdir"
+        fi
+	echo "mounting $1:$export at $mountdir"
+	sudo mount "$1:$export" "$mountdir"
+done
