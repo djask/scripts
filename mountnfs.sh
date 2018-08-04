42a2e86703b42db5e422e480a02679f72df1844a
Richard <djaskeen@gmail.com>
2018-08-04 09:19:31 +0000

    fixed

--- mountnfs.sh
+++ mountnfs.sh
@@ -5,14 +5,17 @@
 #all directories created in the specified mount path
 #to delete, simply remove your specified mountpath folder
 
+basepath=$2
+if [ -z "$2" ]; then
+	echo "using default /export"
+	basepath="/export"
+fi
+
 showmount -e $1 | awk '{if(NR>1)print}' | sed "s/\*//g" | while read -r export ; do
-	mountdir="$2/$(echo $1$export | sed -e 's/\//_/g')"
+	mountdir="$basepath/$(echo $1$export | sed -e 's/\//_/g')"
         if [ ! -d "$mountdir" ]; then
                 mkdir -p "$mountdir"
         fi
-	echo "mount $1:$export at $mountdir? (y/n) "
-	read choice	
-	if [ $choice == 'y' ]; then
-		sudo mount -v -t nfs -o proto=tcp,port=2049,async,rw,vers=4.0 "$1:$export" "$mountdir"
-	fi
+	echo "mount $1:$export at $mountdir"
+	sudo mount -v -t nfs -o proto=tcp,port=2049,async,rw,vers=4.0 "$1:$export" "$mountdir"
 done
